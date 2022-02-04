72e5242f634f7f9c25a43cd1448232b888c5cb2c
Ruslan ALiev <rusik_aliev_2002@mail.ru>
2022-02-04 17:32:26 +0300

    Initial Commit

---
+++ Friends/Contr/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by Руслан Алиев on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var surnameLabel: UILabel!
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    @IBAction func goButton(_ sender: Any) {
+        
+
+    }
+
+    
+    
+    
+
+    
+
+
+
+
---
+++ Friends/Mod/Friends.swift
@@ -0,0 +1,33 @@
+//
+//  Friends.swift
+//  Friends
+//
+//  Created by Руслан Алиев on 03.02.2022.
+//
+
+import Foundation
+
+struct Friends {
+    var name: String
+    var surnname: String
+    var description: String
+}
+
+extension Friends {
+    static var testData = [
+        Friends(name: "Murad", surnname: "Magomedov", description: "👅"),
+        Friends(name: "Bulat", surnname: "Gadgiev", description: "🍗"),
+        Friends(name: "Ali", surnname: "Shamhalov", description: "💨"),
+        Friends(name: "Maga", surnname: "Magomedov", description: "👨🏻‍⚕️"),
+        Friends(name: "Sulik", surnname: "Bahmudkadiev", description: "🧟‍♂️"),
+        Friends(name: "Gamzat", surnname: "Murtazaliev", description: "🚔"),
+        Friends(name: "Karim", surnname: "Shikhaminov", description: "♣️"),
+        Friends(name: "Rustam", surnname: "Dolmatov", description: "🧑🏽‍💻"),
+        Friends(name: "Nabi", surnname: "Nabiev", description: "💪🏼"),
+        Friends(name: "Marat", surnname: "Saidov", description: "🚶🏻"),
+        Friends(name: "Nariman", surnname: "Shamilov", description: "🦅"),
+        Friends(name: "Isa", surnname: "Farmanov", description: "👫"),
+        ]
+}
+
+
